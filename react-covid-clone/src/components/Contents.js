a335174b17122ea21c06243200404c67f9a5413e
MINDA01 <ekdud4639@gmail.com>
2021-09-05 22:35:09 +0900

    feat: prettier 적용

--- react-covid-clone/src/components/Contents.js
+++ react-covid-clone/src/components/Contents.js
@@ -1,72 +1,74 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { Bar, Doughnut, Line } from 'react-chartjs-2';
 import axios from 'axios';
 
 const Contents = () => {
+  const [confirmedData, setConfirmedData] = useState({
+    labels: ['1월', '2월', '3월'],
+    datasets: [
+      {
+        label: '국내 누적 확진자',
+        backgroundColor: 'salmon',
+        fill: true,
+        data: [10, 5, 3],
+      },
+    ],
+  });
 
-    const [ confirmedData, setConfirmedData] = useState({
-        labels: ["1월", "2월","3월"],
-        datasets: [
-            { 
-                label: "국내 누적 확진자" ,
-                backgroundColor: "salmon",
-                fill: true,
-                data: [10,5,3]
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const res = await axios.get('https://api.covid19api.com/total/dayone/country/kr');
+      makeData(res.data);
+    };
+    const makeData = (items) => {
+      const arr = items.reduce((acc, cur) => {
+        const currentDate = new Date(cur.Date);
+        const year = currentDate.getFullYear();
+        const month = currentDate.getMonth();
+        const date = currentDate.getDate();
+        const confirmed = cur.Confirmed;
+        const active = cur.Active;
+        const death = cur.Deaths;
+        const recovered = cur.Recovered;
+
+        const findItem = acc.find((a) => a.year === year && a.month === month);
+
+        if (!findItem) {
+          acc.push({ year, month, date, confirmed, active, death, recovered });
+        }
+        if (findItem && findItem.data < date) {
+          findItem.active = active;
+          findItem.death = death;
+          findItem.date = date;
+          findItem.year = year;
+          findItem.month = month;
+          findItem.recovered = recovered;
+          findItem.confirmed = confirmed;
+        }
+        return acc;
+      }, []);
+
+      console.log(arr);
+    };
+
+    fetchEvents();
+  });
+  return (
+    <section>
+      <h2>국내 코로나 현황</h2>
+      <div className="contents">
+        <div>
+          <Bar
+            data={confirmedData}
+            options={
+              ({ title: { display: true, text: '누적 확진자 추이', fontSize: 16 } },
+              { legend: { display: true, position: 'bottom' } })
             }
-        ]
-    })
-
-    useEffect( ()=> {
-        const fetchEvents = async () => {
-            const res = await axios.get('https://api.covid19api.com/total/dayone/country/kr');
-            makeData(res.data)
-        }
-        const makeData = (items) => {
-            const arr = items.reduce((acc, cur) => {
-                const currentDate = new Date(cur.Date);
-                const year = currentDate.getFullYear();
-                const month = currentDate.getMonth();
-                const date = currentDate.getDate();
-                const confirmed = cur.Confirmed;
-                const active = cur.Active;
-                const death = cur.Deaths;
-                const recovered = cur.Recovered;
-
-                const findItem = acc.find(a=>a.year === year && a.month === month);
-
-                if(!findItem){
-                    acc.push({ year, month, date, confirmed, active, death, recovered})
-                }
-                if(findItem && findItem.data < date) {
-                    findItem.active = active;
-                    findItem.death = death;
-                    findItem.date = date;
-                    findItem.year = year;
-                    findItem.month = month;
-                    findItem.recovered = recovered;
-                    findItem.confirmed = confirmed;
-                }
-                return acc;
-            }, [])
-
-            console.log(arr)
-        }
-
-        fetchEvents();
-    })
-    return (
-        <section>
-        <h2>국내 코로나 현황</h2>
-        <div className="contents">
-            <div>
-                <Bar data={confirmedData} options= {
-                    { title: { display: true, text: "누적 확진자 추이", fontSize: 16} },
-                    { legend: {display: true, position: "bottom"} }
-                } />
-            </div>
+          />
         </div>
-      </section>
-    )
-}
+      </div>
+    </section>
+  );
+};
 
-export default Contents
+export default Contents;
--- react-covid-clone/src/components/Header.js
+++ react-covid-clone/src/components/Header.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
-    return (
-        <header className="header">
-        <h1>COVEID-19</h1>
-        <select>
-          <option>국내</option>
-          <option>국외</option>
-        </select>
-      </header>
-    )
-}
+  return (
+    <header className="header">
+      <h1>COVEID-19</h1>
+      <select>
+        <option>국내</option>
+        <option>국외</option>
+      </select>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
